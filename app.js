43ac490788b1eb70a7b061c859df55d6d9523268
dungbangj <yoyob1223@gmail.com>
2022-02-17 23:54:35 +0900

    camp CRUD

--- app.js
+++ app.js
@@ -1,37 +1,75 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
-// mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connect('mongodb://0.0.0.0:27017/yelpCampDb')
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("Error, MONGO CONNECTION!!!!")
+        console.log(err)
+    })
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useNewUrlParser: true,
-    // useCreateIndex: true         deprecated
-    useUnifiedTopology: true
-});
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
-});
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error!!!!"));
+// db.once("open", () => {
+//     console.log("Database connected");
+// });
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+//req.body를 parse시킨다.
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/campground', async (req, res) => {
-    const camp = new Campground({title: 'My Backyard', description: 'cheap camping!'});
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
+});
+//:id뒤에 위치하면 뒤에 나오는 함수들은 모두 id로 취급해서 화면상에 출력이 되지 않는다.
+app.get('/campgrounds/new', async (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', {campground});
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground});
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const{id} = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const{id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
 });
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
 })
+
---
+++ seeds/index.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Campground = require('../models/campground');
+const {places, descriptors} = require('./seedHelper');
+const cities = require('./cities');
+
+mongoose.connect('mongodb://localhost:27017/yelpCampDb')
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("Error, MONGO CONNECTION!!!!")
+        console.log(err)
+    })
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error!!!!"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const rand1000 = Math.floor(Math.random() * 1000);
+        const camp = new Campground({
+            location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`
+        })
+        await camp.save();
+    }
+};
+
+seedDB().then(() => {
+    mongoose.connection.close();
+});
