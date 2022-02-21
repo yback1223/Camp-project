c246066e6a2cdae16d51f58abc6d4aa8b6ff17a5
dungbangj <yoyob1223@gmail.com>
2022-02-21 23:50:04 +0900

    error handling

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressErrors');
+const {campgroundSchema} = require('./schemas');
 
 mongoose.connect('mongodb://0.0.0.0:27017/yelpCampDb')
     .then(() => {
@@ -30,46 +33,88 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'));
 
+const validateCampground = (req, res, next) => {
+
+    const {error} = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next()
+    }
+}
+
+
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res, next) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
-});
-//:id뒤에 위치하면 뒤에 나오는 함수들은 모두 id로 취급해서 화면상에 출력이 되지 않는다.
-app.get('/campgrounds/new', async (req, res) => {
-    res.render('campgrounds/new');
-});
+}))
 
-app.post('/campgrounds', async (req, res) => {
+//:id뒤에 위치하면 뒤에 나오는 함수들은 모두 id로 취급해서 화면상에 출력이 되지 않는다.
+app.get('/campgrounds/new', catchAsync(async (req, res, next) => {
+    res.render('campgrounds/new');
+}))
+
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+    // if(!req.campground) throw new ExpressErrors('Invalid Campground data', 400);
+    // const campgroundSchema = Joi.object({
+    //     campground: Joi.object({
+    //         title: Joi.string().required(),
+    //         price: Joi.number().required().min(0),
+    //         image: Joi.string().required(),
+    //         location: Joi.string().required(),
+    //         description: Joi.string().required()
+    //     }).required()
+    // })
+    // const {error} = campgroundSchema.validate(req.body);
+    // if (error) {
+    //     const msg = error.details.map(el => el.message).join(',');
+    //     throw new ExpressErrors(msg, 400);
+    // }
+    // console.log(result);
+
+
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}));
 
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {campground});
-});
+}))
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', {campground});
-});
+}))
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res, next) => {
     const{id} = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
     res.redirect(`/campgrounds/${campground._id}`);
-});
+}))
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res, next) => {
     const{id} = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-});
+}))
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not Found', 404));
+})
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500} = err;
+    if(!err.message) err.message = 'Oh no, Error!!'
+    res.status(statusCode).render('error', {err});
+})
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
